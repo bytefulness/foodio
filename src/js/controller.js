21bb666e439512f0da6e9f533ec43e13b9ffa6b6
eakyel <e.akyel@outlook.com>
2021-03-22 22:33:48 +0300

    Create MVC architecture and Load recipe from API
    For MVC architecture created model, controller and view component files.

---
+++ src/js/controller.js
@@ -0,0 +1,32 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    // Throw error when fetch status false
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // # Change object keyname
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+showRecipe();
