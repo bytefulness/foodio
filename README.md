3f6463c66c908209d1144bcebef3d9c4e16dea65
eakyel <e.akyel@outlook.com>
2021-04-22 16:49:37 +0300

    Get build and add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Foodio
+
+Search for the recipe from over 1,000,000 recipes for your dinner.
+
+![Project Screenshot](/screenshot.png)
+
+## Preview
+
+See the [live version](https://foodio-app.netlify.app/) of the project.
+
+## Known Bugs
+
+No bugs so far. If found, email me at [e.akyel@outlook.com](mailto:e.akyel@outlook.com)
--- dist/controller.2d2f8cd0.js
+++ dist/controller.2d2f8cd0.js
@@ -4111,8 +4111,6 @@ const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
   const start = (page - 1) * state.search.resultsPerPage;
   const end = page * state.search.resultsPerPage;
-  console.log(start, end);
-  console.log(state.search.results);
   return state.search.results.slice(start, end);
 };
 
